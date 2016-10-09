57efa4f9cfbf0551bc85fdba6fe8c9d9f3c33245
Joyce&Luna <joyceLuna@online.de>
2016-10-10 00:26:14 +0200

    New
    Links are now inserted in the ACP

---
+++ acp/main_info.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+*
+* @package phpBB Extension - Footerlinks
+* @copyright (c) 2016 joyceluna (https://phpbb-style-design.de)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @ver 1.3.0
+*
+*/
+
+/**
+* @ignore
+*/
+
+/**
+* @package module_install
+*/
+
+namespace joyceluna\footerlinks\acp;
+
+class main_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> '\joyceluna\footerlinks\acp\main_module',
+			'title'		=> 'ACP_FOOTERLINKS_TITLE',
+			'version'	=> 'footerlinks_version',
+			'modes'		=> array(
+				'settings'	=> array(
+					'title' 	=> 'ACP_FOOTERLINKS_CONFIG',
+					'auth' 		=> 'joyceluna/footerlinks && acl_a_board',
+					'cat'		=> array('ACP_FOOTERLINKS_CONFIG')),
+				),
+		);
+	}
+}
+
+?>
--- event/listener.php
+++ event/listener.php
@@ -1,9 +1,10 @@
 <?php
 /**
 *
-* @package phpBB Extension - Footer Links
+* @package phpBB Extension - Footerlinks
 * @copyright (c) 2016 joyceluna (https://phpbb-style-design.de)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @ver 1.3.0
 *
 */
 namespace joyceluna\footerlinks\event;
@@ -21,31 +22,38 @@ class listener implements EventSubscriberInterface
 	public static function getSubscribedEvents()
 	{
 		return array(
-			'core.page_header'      => 'page_header',
+			'core.user_setup'						=> 'load_language_on_setup',
+			'core.page_header'			=> 'footerlinks',
 		);
 	}
 
-	/** @var \phpbb\user */
-	protected $user;
+	/* @var \phpbb\controller\helper */
+	protected $helper;
 
-	/**
-	 * Constructor
-	 *
-	 * @param \phpbb\user       $user       User object
-	 */
-	public function __construct(\phpbb\user $user)
+	/* @var \phpbb\template\template */
+	protected $template;
+
+
+	public function load_language_on_setup($event)
 	{
-		$this->user = $user;
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'joyceluna/footerlinks',
+			'lang_set' => 'lang_footerlinks',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
 	}
-	/**
-	 * Load language file
-	 *
-	 * @param    object  $event  The event object
-	 * @return   null
-	 * @access   public
-	 */
+
 	public function page_header($event)
 	{
-		$this->user->add_lang_ext('joyceluna/footerlinks', 'main');
+		$this->user->add_lang_ext('joyceluna/footerlinks', 'lang_footerlinks');
 	}
+	
+	public function footerlinks($event)
+	{
+	
+		include 'ext/joyceluna/footerlinks/core/footerlinks.php';
+	}
+	
 }
+?>
