a9264d203558b726e94d8e09ad2025fcd70e6feb
Joyce&Luna <joyceLuna@online.de>
2016-10-10 04:33:13 +0200

    Update footerlinks.php
    Remove empty lines

--- core/footerlinks.php
+++ core/footerlinks.php
@@ -19,7 +19,7 @@ global $phpbb_root_path, $phpEx, $db, $auth, $user, $template;
 	$sql = 'SELECT fl_enable,fl_ext_link,fl_enable_b1,fl_enable_b2,fl_enable_b3,fl_title_cat1,fl_title_cat2,fl_title_cat3
 	FROM '.'phpbb_footerlinks'.'
 	WHERE footerlinks_id = "1"';
-	
+
 	$result = $db->sql_query_limit($sql, 1);
 	$fl_data = $db->sql_fetchrow($result);
 	$db->sql_freeresult($result);
@@ -27,8 +27,8 @@ global $phpbb_root_path, $phpEx, $db, $auth, $user, $template;
 	if ($fl_data['fl_enable'])
 	{ 
 		$template->assign_vars(array(
-    		'FL_ENABLE'			=> $fl_data['fl_enable'],
-		    'FL_EXT_LINK'		=> $fl_data['fl_ext_link'],
+			'FL_ENABLE'			=> $fl_data['fl_enable'],
+			'FL_EXT_LINK'		=> $fl_data['fl_ext_link'],
 			'FL_ENABLE_B1'		=> $fl_data['fl_enable_b1'],
 			'FL_ENABLE_B2'		=> $fl_data['fl_enable_b2'],
 			'FL_ENABLE_B3'		=> $fl_data['fl_enable_b3'],
@@ -38,11 +38,11 @@ global $phpbb_root_path, $phpEx, $db, $auth, $user, $template;
 		));
 //Block 1
 		if ($fl_data['fl_enable_b1']) 
-		{ 		
+		{
 			$sql = 'SELECT fl_title_cat1,fl_link1, fl_link_text1
 			FROM ' . 'phpbb_footerlinks';
 			$result = $db->sql_query($sql);
-		
+
 			while ($row = $db->sql_fetchrow($result))
 			{
 			if (!empty($row['fl_link1']))
@@ -60,11 +60,11 @@ global $phpbb_root_path, $phpEx, $db, $auth, $user, $template;
 			$sql = 'SELECT fl_title_cat2,fl_link2, fl_link_text2
 			FROM ' . 'phpbb_footerlinks';
 			$result = $db->sql_query($sql);
-		
+
 			while (!empty($row = $db->sql_fetchrow($result)))
 			{
 				if (!empty($row['fl_link2']))
-				{	
+				{
 					$template->assign_block_vars('fl_links2', array(
 					'FL_LINK2'			=> $row['fl_link2'],
 					'FL_LINK_TEXT2'		=> $row['fl_link_text2'],
@@ -78,11 +78,11 @@ global $phpbb_root_path, $phpEx, $db, $auth, $user, $template;
 			$sql = 'SELECT fl_title_cat3,fl_link3, fl_link_text3
 			FROM ' . 'phpbb_footerlinks';
 			$result = $db->sql_query($sql);
-	
+
 			while ($row = $db->sql_fetchrow($result))
 			{
 				if (!empty($row['fl_link3']))
-				{	
+				{
 					$template->assign_block_vars('fl_links3', array(
 						'FL_LINK3'			=> $row['fl_link3'],
 						'FL_LINK_TEXT3'		=> $row['fl_link_text3'],
