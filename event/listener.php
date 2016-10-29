77a383ee54b8127cda63c5e8d9b4c06b58867231
Joyce&Luna <joyceLuna@online.de>
2016-10-29 18:39:15 +0200

    Add files via upload

--- event/listener.php
+++ event/listener.php
@@ -4,7 +4,7 @@
 * @package phpBB Extension - Footerlinks
 * @copyright (c) 2016 joyceluna (https://phpbb-style-design.de)
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-* @ver 1.4.0
+* @ver 1.3.2
 *
 */
 namespace joyceluna\footerlinks\event;
@@ -19,6 +19,53 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var string */
+	protected $phpbb_root_path;
+
+	/** @var string */
+	protected $php_ext;
+
+	/** @var string */
+	protected $ext_name;
+	
+	/** @var string */
+ 	protected $table_prefix;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\template\template          	$template
+	* @param \phpbb\user                       	$user
+	* @param \phpbb\db\driver\driver_interface 	$db
+	* @param \phpbb\request\request            	$request
+	* @param string                            	$phpbb_root_path
+	* @param string                            	$php_ext
+	* @param string								$table_prefix
+	*/
+
+	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, $phpbb_root_path, $php_ext, $table_prefix)
+	{
+		$this->template = $template;
+		$this->user = $user;
+		$this->db = $db;
+		$this->request = $request;
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext = $php_ext;
+		$this->table_prefix = $table_prefix;
+	}
+
 	public static function getSubscribedEvents()
 	{
 		return array(
@@ -26,13 +73,7 @@ class listener implements EventSubscriberInterface
 			'core.page_header'			=> 'footerlinks',
 		);
 	}
-
-	/* @var \phpbb\controller\helper */
-	protected $helper;
-
-	/* @var \phpbb\template\template */
-	protected $template;
-
+	
 	public function load_language_on_setup($event)
 	{
 		$lang_set_ext = $event['lang_set_ext'];
@@ -50,19 +91,21 @@ class listener implements EventSubscriberInterface
 
 	public function footerlinks($event)
 	{
-		global $config, $db, $user, $auth, $template, $phpbb_root_path, $table_prefix;
-
-		$footerlinks_table = $table_prefix . 'footerlinks';
+		if (!defined('FOOTERLINKS')) 
+		{ 
+			$footerlinks_table = $this->table_prefix . 'footerlinks'; 
+ 			define('FOOTERLINKS', $footerlinks_table); 
+ 		} 
 
 		$sql = 'SELECT * 
 		FROM '. $footerlinks_table;
 
-		$result = $db->sql_query($sql);
-		$fl_data = $db->sql_fetchrow($result);
+		$result = $this->db->sql_query($sql);;
+		$fl_data = $this->db->sql_fetchrow($result);
 
 		if ($fl_data['fl_enable'])
 		{
-			$template->assign_vars(array(
+			$this->template->assign_vars(array(
 				'FL_ENABLE'			=> $fl_data['fl_enable'],
 				'FL_EXT_LINK'		=> $fl_data['fl_ext_link'],
 				'FL_ENABLE_B1'		=> $fl_data['fl_enable_b1'],
@@ -73,13 +116,13 @@ class listener implements EventSubscriberInterface
 				'FL_TITLE_CAT3'		=> $fl_data['fl_title_cat3'],
 			));
 
-			while ($row = $db->sql_fetchrow($result))
+			while ($row = $this->db->sql_fetchrow($result))
 			{
 				if ($fl_data['fl_enable_b1'])
 				{
 					if (!empty($row['fl_link1']))
 					{
-						$template->assign_block_vars('fl_links1', array(
+						$this->template->assign_block_vars('fl_links1', array(
 						'FL_LINK1'			=> $row['fl_link1'],
 						'FL_LINK_TEXT1'		=> $row['fl_link_text1'],
 						));
@@ -90,7 +133,7 @@ class listener implements EventSubscriberInterface
 				{
 					if (!empty($row['fl_link2']))
 					{
-						$template->assign_block_vars('fl_links2', array(
+						$this->template->assign_block_vars('fl_links2', array(
 							'FL_LINK2'			=> $row['fl_link2'],
 						'FL_LINK_TEXT2'		=> $row['fl_link_text2'],
 						));
@@ -101,7 +144,7 @@ class listener implements EventSubscriberInterface
 				{
 					if (!empty($row['fl_link3']))
 					{
-						$template->assign_block_vars('fl_links3', array(
+						$this->template->assign_block_vars('fl_links3', array(
 							'FL_LINK3'			=> $row['fl_link3'],
 							'FL_LINK_TEXT3'		=> $row['fl_link_text3'],
 						));
@@ -109,6 +152,6 @@ class listener implements EventSubscriberInterface
 				}
 			}
 		}
-		$db->sql_freeresult($result);
+		$this->db->sql_freeresult($result);
 	}
 }
