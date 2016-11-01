fac8c770f8c6fba8f8f99f0dcff395efb4fe5bbb
Joyce&Luna <joyceLuna@online.de>
2016-11-01 23:17:28 +0100

    Update init.php

--- migrations/init.php
+++ migrations/init.php
@@ -68,18 +68,7 @@ class init extends \phpbb\db\migration\migration
 				'module_mode'		=> 'overview',
 				'module_auth'		=> 'ext_joyceluna/footerlinks && acl_a_board',
 			))),
-
-		// ADD VALUES => DB
-		array('custom', array(array(&$this, 'add_footerlinks_data'))),
 		);
-	}
-
-	public function add_footerlinks_data()
-	{
-		$footerlinks_sql_query = array(
-		'footerlinks_id'	=> '1',
-		);
-		$this->db->sql_multi_insert($this->table_prefix . 'footerlinks', $footerlinks_sql_query);
 	}
 
 	/**
