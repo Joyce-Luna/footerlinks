f4a0256951eb348ece9b81bf0274171a7bbc3f3c
Joyce&Luna <joyceLuna@online.de>
2017-09-08 16:46:40 +0200

    Update prepare-epv.sh

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -1,24 +1,20 @@
-#!/bin/bash
+ #!/bin/bash
 #
-# This file is part of the phpBB Forum Software package.
+# PayPal Donation extension for the phpBB Forum Software package.
 #
-# @copyright (c) phpBB Limited <https://www.phpbb.com>
+# @copyright (c) 2016 Matt Friedman
 # @license GNU General Public License, version 2 (GPL-2.0)
-#
-# For full copyright and license information, please see
-# the docs/CREDITS.txt file.
 #
 set -e
 set -x
 
-EXTNAME=$1
-BRANCH=$2
-EXTPATH_TEMP=$3
+DB=$1
+TRAVIS_PHP_VERSION=$2
 
-# Copy extension to a temp folder
-mkdir ../../tmp
-cp -R . ../../tmp
-cd ../../
-
-# Clone phpBB
-git clone --depth=1 "git://github.com/phpbb/phpbb.git" "phpBB3" --branch=$BRANCH
+if [ "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.5" ]
+then
+	cd phpBB
+	composer remove sami/sami --update-with-dependencies --dev --no-interaction
+	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
+	cd ../
+fi
